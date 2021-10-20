6135e34f6e935f1872bccade43fb44491b3e7476
Cisaa <75185984+Cisaa@users.noreply.github.com>
2021-10-20 20:15:40 +0200

    Fixed client.event bug and bot.command bug. Bot fully works (update 0.1.1)

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
 import discord
 from discord.ext import commands
-
+from discord.ext.commands import bot
 
 client = commands.Bot(command_prefix = [">"]) # creating bot variable with prefixes
 
-@client.event()
+@client.event
 async def on_ready():
-    print('Logged on as {0}!'.format(self.user))
+    print('We have logged in as {0.user}'
+    .format(client))
     await client.change_presence(status=discord.Status.online, activity = discord.Activity(type=discord.ActivityType.watching, name="your commands"))
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
 
-@bot.command()
+
+@client.command()
 async def about(ctx):
      await ctx.send('Hi! This is DynamicDevs first project! Dont expect much but hi')
 
 
 
 
-@bot.command()
-async def delta(ctx, *, member: JoinDistanceConverter):
-    is_new = member.delta.days < 100
-    if is_new:
-        await ctx.send("Hey you're pretty new!")
-    else:
-        await ctx.send("Hm you're not so new.")
-@bot.command()
-async def joined(ctx, *, member: discord.Member):
-    await ctx.send('{0} joined on {0.joined_at}'.format(member))
-
-client = MyClient()
 client.run('OTAwNDQwNDQxMjM1ODQ1MjMx.YXBWgg.eSn7YAouWFj_ngOj6kYU22j1uHk')
