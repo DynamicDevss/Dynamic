b69093a4c77ead1a4823cabac5cdd5152d350529
Cisaa <75185984+Cisaa@users.noreply.github.com>
2021-10-20 19:44:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import bot
 
 
 class MyClient(discord.Client):
@@ -8,14 +9,24 @@ class MyClient(discord.Client):
 
 client= commands.Bot(command_prefix = '<')
 
-@client.command()
+@bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-@client.command()
+@bot.command()
 async def about(ctx):
      await ctx.send('Hi! This is DynamicDevs first project! Dont expect much but hi')
 
+@bot.command()
+async def delta(ctx, *, member: JoinDistanceConverter):
+    is_new = member.delta.days < 100
+    if is_new:
+        await ctx.send("Hey you're pretty new!")
+    else:
+        await ctx.send("Hm you're not so new.")
+@bot.command()
+async def joined(ctx, *, member: discord.Member):
+    await ctx.send('{0} joined on {0.joined_at}'.format(member))
 
 client = MyClient()
 client.run('dontgetmytokenprick')
