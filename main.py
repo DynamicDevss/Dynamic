0d6fe302ff75aa260bf8f53465c700f82dbb933c
Cisaa <75185984+Cisaa@users.noreply.github.com>
2021-10-20 20:57:58 +0200

    Added kick / ban commands. (update: 0.2.0)
    >kick <user> <reason>
    >ban <user> <reason>

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity = discord.Activity(type=discord.ActivityType.watching, name="your commands"))
 
 
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
 
 @client.command()
 async def about(ctx):
