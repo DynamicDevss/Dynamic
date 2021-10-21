bb496d00930eca4495190ba06c5813a24798e3c9
Lukas <76885856+LJ-exe@users.noreply.github.com>
2021-10-21 20:20:37 +0100

    Fixed cisas crappy code

--- cogs/music.py
+++ cogs/music.py
@@ -16,17 +16,15 @@ class music(commands.Cog):
     async def on_ready(self):
         print('Cog #1 is up')
 
-
-
-
-
-
-
-@commands.command(pass_context=True)
-async def play(self, ctx, url):
+    @commands.command(pass_context=True)
+    async def play(self, ctx, url):
        guild = ctx.message.guild
        voice_client = guild.voice_client
        player = await voice_client.create_ytdl_player(url)
        players[server.id] = player
        player.start()
 
+        
+        
+def setup(client):
+    client.add_cog(music(client))
